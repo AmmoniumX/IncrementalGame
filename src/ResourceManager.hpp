21e82fe062a285ac27b71d1c7ca0e3a8ec85d4ea
Diego Garcia <diego.garcia.cr@gmail.com>
2025-08-10 21:33:05 -0600

    put Resource internals inside 'detail' namespace

--- src/ResourceManager.hpp
+++ src/ResourceManager.hpp
@@ -1,14 +1,10 @@
 #pragma once
 
 #include <algorithm>
-#include <iostream>
 #include <print>
 #include <format>
-#include <functional>
-#include <optional>
 #include <string>
 #include <string_view>
-#include <stdexcept>
 #include <unordered_set>
 #include <unordered_map>
 #include <memory>
@@ -20,41 +16,37 @@
 
 using nlohmann::json;
 
-class _Resource {
+namespace detail{
+class Resource {
 public:
-    virtual ~_Resource() = default;
+    virtual ~Resource() = default;
 
     virtual json serialize() const = 0;
     virtual void deserialize(const json& j) = 0;
 
-    _Resource(const _Resource&) = delete;
-    _Resource& operator=(const _Resource&) = delete;
+    Resource(const Resource&) = delete;
+    Resource& operator=(const Resource&) = delete;
 
 protected:
-    _Resource() = default;
+    Resource() = default;
 };
+} // namespace detail
 
-using Resource = boost::synchronized_value<std::unique_ptr<_Resource>>;
+using Resource = boost::synchronized_value<std::unique_ptr<detail::Resource>>;
 
 class ResourceManager {
 private:
     ResourceManager() = default;
-
-    // // Container of uniquely owned resources
-    // std::unordered_map<std::string, std::shared_ptr<_Resource>> resources;
-
-    // // Container of shared pointers to resources, allowing thread-safe access
-    // std::unordered_map<std::string, std::shared_ptr<boost::synchronized_value<_Resource*>>> owned_synchronized_resources;
     
     std::unordered_map<std::string, std::shared_ptr<Resource>> resources;
     std::mutex mtx;
 
-    std::shared_ptr<Resource> newResource(std::unique_ptr<_Resource> &&r) {
-        return std::make_shared<Resource>(boost::synchronized_value<std::unique_ptr<_Resource>>(std::move(r)));
+    std::shared_ptr<Resource> newResource(std::unique_ptr<detail::Resource> &&r) {
+        return std::make_shared<Resource>(boost::synchronized_value<std::unique_ptr<detail::Resource>>(std::move(r)));
     }
 
-    std::shared_ptr<Resource> newResource(_Resource *r) {
-        return std::make_shared<Resource>(boost::synchronized_value<std::unique_ptr<_Resource>>(std::unique_ptr<_Resource>(r)));
+    std::shared_ptr<Resource> newResource(detail::Resource *r) {
+        return std::make_shared<Resource>(boost::synchronized_value<std::unique_ptr<detail::Resource>>(std::unique_ptr<detail::Resource>(r)));
     }
 
 public:
@@ -72,14 +64,14 @@ public:
         return result;
     }
 
-    void create(const std::string_view _id, _Resource *resource) {
+    void create(const std::string_view _id, detail::Resource *resource) {
         std::println(stderr, "Creating resource: {}", _id);
         std::lock_guard lock(mtx);
         std::string id(_id);
         resources.insert_or_assign(id, newResource(resource));
     }
 
-    void create(const std::string_view _id, std::unique_ptr<_Resource> &&resource) {
+    void create(const std::string_view _id, std::unique_ptr<detail::Resource> &&resource) {
         std::println(stderr, "Creating resource: {}", _id);
         std::lock_guard lock(mtx);
         std::string id(_id);
@@ -144,7 +136,7 @@ public:
 
 // CRTP helper for automatic registration
 template <typename Derived>
-class RegisteredResource : public _Resource {
+class RegisteredResource : public detail::Resource {
 public:
     RegisteredResource() = default;
 
