f2f3b8a9e24c61be773036c778d290af305b7663
Diego Garcia <diego.garcia.cr@gmail.com>
2025-08-16 12:45:22 -0600

    More explicit macro targetting fixed-size chars

--- src/render/wcwidth.hpp
+++ src/render/wcwidth.hpp
@@ -10,17 +10,17 @@ int mk_uswidth(const std::u16string_view u16s);
 #if defined(_WIN32)
 // On Windows, wchar_t is always 16 bits
 using ustring = std::u16string;
-#define WCHAR_WIDTH 2
+#define WCHAR_EQUIVALENT char16_t
 #elif defined(__linux__) || defined(__APPLE__) || defined(__unix__)
 // On most Unix-like systems, wchar_t is 32 bits
 using ustring = std::u32string;
-#define WCHAR_WIDTH 4
+#define WCHAR_EQUIVALENT char32_t
 #else
 #error "Unsupported platform for wchar_t width detection."
 #endif
 
 // Sanity check
-static_assert(sizeof(wchar_t) == WCHAR_WIDTH, "Nonmatching WCHAR_WIDTH");
+static_assert(sizeof(wchar_t) == sizeof(WCHAR_EQUIVALENT), "Nonmatching WCHAR_EQUIVALENT");
 
 inline ustring ustring_from_wstring(const std::wstring_view ws) {
     return ustring(std::from_range, ws);
